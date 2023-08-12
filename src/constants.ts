82cd01f54bff404440f160c1730322f0e94f1dfa
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2023-08-13 00:18:21 +0100

    Adds service caching

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export enum STORAGE_KEYS {
 	LOGGED_USER = 'logged-user',
+	CACHED_ANIMALS = 'cached-animals',
 }
--- src/services/animals.ts
+++ src/services/animals.ts
@@ -1,14 +1,30 @@
+import { STORAGE_KEYS } from '../constants';
 import { TAnimal } from '../types';
 
 const BASE_URL = 'https://api-ninjas.com/api/animals';
 
 export async function getAnimals(name: string): Promise<TAnimal[] | Error> {
+	const cachedAnimals: Record<string, TAnimal | undefined> = JSON.parse(
+		window.localStorage.get(STORAGE_KEYS.CACHED_ANIMALS) || '{}'
+	);
+	const cached = cachedAnimals[name];
+
+	if (cached) return [cached];
+
 	try {
 		const res = await fetch(`${BASE_URL}?name=${name}`, {headers: {'X-Api-Key': import.meta.env.VITE_API_KEY}});
 		if (!res.ok) {
 			throw new Error('Failed to fetch');
 		}
-		return res.json();
+		const animals: TAnimal[] = await res.json();
+
+		animals.forEach(animal => {
+			cachedAnimals[animal.name] = animal;
+		});
+		window.localStorage
+			.setItem(STORAGE_KEYS.CACHED_ANIMALS, JSON.stringify(cachedAnimals));
+
+		return animals;
 	} catch (e) {
 		return e as Error;
 	}
